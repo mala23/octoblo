26e2f7057eaacb9417d58a76385c7434bd3db9f2
mala23 <mail@mala23.me>
2020-12-02 21:29:40 +0100

    implement ultrasonic distance sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,34 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ESP32Servo.h>
+#include <NewPing.h>
 
-/*
+#define SONAR_NUM 1      // Number of sensors.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
+
+NewPing sonar[SONAR_NUM] = {   // Sensor object array.
+  NewPing(12, 13, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
+};
+
 const char* ssid     = "octoblo";
 const char* password = "Bluish-Octavo-82";
-*/
+/*
 const char* ssid     = "kali";
 const char* password = "hoiduuu<3";
+*/
 
 //const char* mqtt_server = "mqtt://3d8f57f8:67f1ba13358e9b30@broker.shiftr.io";
 //const char* mqtt_server = "192.168.1.25";
-const char* mqtt_server = "172.20.10.3";
+//const char* mqtt_server = "172.20.10.3";
+const char* mqtt_server = "192.168.1.42";
 
-//const char *ID = "octoswitch01";
-const char *ID = "octoswitch02";
-const char *BLOWERS_TOPIC = "octoblo/blowers";
+const char *ID = "octoeye";
+const char *EYE_TOPIC = "octoblo/eye";
 
-const int BLO1 = 15;
-const int BLO2 = 2;
-const int BLO3 = 4;
-const int BLO4 = 5;
-
-const int BLO5 = 26;
-const int BLO6 = 25;
-const int BLO7 = 33;
-const int BLO8 = 32;
-
-bool BLO1_STATE = false;
-bool BLO2_STATE = false;
-bool BLO3_STATE = false;
-bool BLO4_STATE = false;
-
-bool BLO5_STATE = false;
-bool BLO6_STATE = false;
-bool BLO7_STATE = false;
-bool BLO8_STATE = false;
+bool EYE_STATE = false;
 
 WiFiClient wclient;
 PubSubClient client(wclient);
-
-/*
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE(50)
-char msg[MSG_ BUFFER_SIZE];
-int value = 0;
-*/
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String response;
@@ -59,6 +42,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("] ");
   Serial.println(response);
 
+  /*
   if(response == "blo1")
   {
     BLO1_STATE = !BLO1_STATE;
@@ -66,64 +50,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println("blo1_switched");
     Serial.println(BLO1_STATE);
   }
-
-  if(response == "blo2")
-  {
-    BLO2_STATE = !BLO2_STATE;
-    client.publish(BLOWERS_TOPIC, "blo2_switched");
-    Serial.println("blo2_switched");
-    Serial.println(BLO2_STATE);
-  }
-
-  if(response == "blo3")
-  {
-    BLO3_STATE = !BLO3_STATE;
-    client.publish(BLOWERS_TOPIC, "blo3_switched");
-    Serial.println("blo3_switched");
-    Serial.println(BLO3_STATE);
-  }
-
-  if(response == "blo4")
-  {
-    BLO4_STATE = !BLO4_STATE;
-    client.publish(BLOWERS_TOPIC, "blo4_switched");
-    Serial.println("blo4_switched");
-    Serial.println(BLO4_STATE);
-  }
-
-  if(response == "blo5")
-  {
-    BLO5_STATE = !BLO5_STATE;
-    client.publish(BLOWERS_TOPIC, "blo5_switched");
-    Serial.println("blo5_switched");
-    Serial.println(BLO5_STATE);
-  }
-
-  if(response == "blo6")
-  {
-    BLO6_STATE = !BLO6_STATE;
-    client.publish(BLOWERS_TOPIC, "blo6_switched");
-    Serial.println("blo6_switched");
-    Serial.println(BLO6_STATE);
-  }
-
-  if(response == "blo7")
-  {
-    BLO7_STATE = !BLO7_STATE;
-    client.publish(BLOWERS_TOPIC, "blo7_switched");
-    Serial.println("blo7_switched");
-    Serial.println(BLO7_STATE);
-  }
-
-  if(response == "blo8")
-  {
-    BLO8_STATE = !BLO8_STATE;
-    client.publish(BLOWERS_TOPIC, "blo8_switched");
-    Serial.println("blo8_switched");
-    Serial.println(BLO8_STATE);
-  }
+  */
 }
-
 
 void reconnect() {
   // Loop until we're reconnected
@@ -136,9 +64,9 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an annou2cement...
-      client.publish("/octoblo", "switch01 connected");
+      client.publish("/octoblo/eye", "octoeye connected");
       // ... and resubscribe
-      client.subscribe("/octoblo/blowers");
+      client.subscribe(EYE_TOPIC);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -150,30 +78,8 @@ void reconnect() {
 }
 
 void setup() {
-    pinMode(BLO1, OUTPUT);
-    pinMode(BLO2, OUTPUT);
-    pinMode(BLO3, OUTPUT);
-    pinMode(BLO4, OUTPUT);
-
-    pinMode(BLO5, OUTPUT);
-    pinMode(BLO6, OUTPUT);
-    pinMode(BLO7, OUTPUT);
-    pinMode(BLO8, OUTPUT);
-
-    digitalWrite(BLO1, HIGH);
-    digitalWrite(BLO2, HIGH);
-    digitalWrite(BLO3, HIGH);
-    digitalWrite(BLO4, HIGH);
-
-    digitalWrite(BLO5, HIGH);
-    digitalWrite(BLO6, HIGH);
-    digitalWrite(BLO7, HIGH);
-    digitalWrite(BLO8, HIGH);
-
-    Serial.begin(115200);
-    delay(10);
-
-    // We start by connecting to a WiFi network
+  Serial.begin(115200);
+    //Connecting to a WiFi network
 
     Serial.println();
     Serial.println();
@@ -195,74 +101,42 @@ void setup() {
 		randomSeed(micros());
     client.setServer(mqtt_server, 1883);
     client.setCallback(callback);
-    client.subscribe("/octoblo/blowers");
+    client.subscribe("EYE_TOPIC");
 }
 
 int value = 0;
 
 void loop() {
 
-  ++value;
+ ++value;
 
-   if (!client.connected())  // Reconnect if connection is lost
-   {
-     reconnect();
-   }
-     client.loop();
-/*
-  digitalWrite(BLO1, LOW);
-  delay(1000);
-  digitalWrite(BLO1, HIGH);
-  delay(1000);
-*/
+ if (!client.connected())  // Reconnect if connection is lost
+ {
+   reconnect();
+ }
+   client.loop();
 
-  // Control Blowers
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
+    delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    char dist = sonar[0].ping_cm();
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(sonar[i].ping_cm());
+    Serial.print("cm ");
+    if (dist != 0) {
+      client.publish("/octoblo/eye", "eye0_switched");
+    }
+  }
+
+  Serial.println();
+
+  /*
+  //Control Eye
   if (BLO1_STATE == true) {
     digitalWrite(BLO1, LOW);
   } else {
     digitalWrite(BLO1, HIGH);
   }
-
-  if (BLO2_STATE == true) {
-    digitalWrite(BLO2, LOW);
-  } else {
-    digitalWrite(BLO2, HIGH);
-  }
-
-  if (BLO3_STATE == true) {
-    digitalWrite(BLO3, LOW);
-  } else {
-    digitalWrite(BLO3, HIGH);
-  }
-
-  if (BLO4_STATE == true) {
-    digitalWrite(BLO4, LOW);
-  } else {
-    digitalWrite(BLO4, HIGH);
-  }
-
-  if (BLO5_STATE == true) {
-    digitalWrite(BLO5, LOW);
-  } else {
-    digitalWrite(BLO5, HIGH);
-  }
-
-  if (BLO6_STATE == true) {
-    digitalWrite(BLO6, LOW);
-  } else {
-    digitalWrite(BLO6, HIGH);
-  }
-
-  if (BLO7_STATE == true) {
-    digitalWrite(BLO7, LOW);
-  } else {
-    digitalWrite(BLO7, HIGH);
-  }
-
-  if (BLO8_STATE == true) {
-    digitalWrite(BLO8, LOW);
-  } else {
-    digitalWrite(BLO8, HIGH);
-  }
+  */
 
 }
